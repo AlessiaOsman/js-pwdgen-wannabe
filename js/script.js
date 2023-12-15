49de41076653d8fd04e892f8b4992aba9a10bc51
AlessiaOsman <alessia.osman@gmail.com>
2023-12-15 15:28:06 +0100

    asked favourite color and changed message value

--- js/script.js
+++ js/script.js
@@ -6,12 +6,20 @@
 // modifico il valore della variabile con valore 21 aggiungendo i dati dell'utente
 // modifico il contenuto testuale
 
-const paragraph = document.getElementById('newPassword')
-console.log(paragraph)
+const paragraph = document.getElementById('newPassword');
+console.log(paragraph);
 
-let message = 'La tua password è'
+let message = 'La tua password è ';
+console.log(message);
+
+let endPassword = '21';
+
+const lastName = prompt('Qual è il tuo cognome?', 'Osman');
+console.log(lastName);
+
+const favouriteColor = prompt('Qual è il tuo colore preferito?', 'Giallo');
+console.log(favouriteColor)
+
+message += lastName + favouriteColor + endPassword;
 console.log(message)
-
-const lastName = prompt('Qual è il tuo cognome', 'Osman')
-console.log(lastName)
 
